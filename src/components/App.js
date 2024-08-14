a4b2fc3b1b7d7bcd8d0e88d6964211647b0c909b
Josh Bain <joshuambain@gmail.com>
2024-08-14 12:01:13 -0400

    complete simple data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
+import {useEffect, useState} from "react"
+
 // create your App component here
+
+function App() {
+    
+    const [imageLink, setImageLink] = useState(null)
+
+    useEffect(() => {
+        
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(image => setImageLink(image.message))
+        
+        // console.log("Hello")
+    },[])
+
+    // console.log("hello")
+
+    if(!imageLink) {
+        return <p>Loading..</p>
+    }
+
+
+    return (
+        <img src={imageLink} alt={"A Random Dog"} />
+    )
+}
+
+export default App;
